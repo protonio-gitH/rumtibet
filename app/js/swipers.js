2be58e2e7bcf569d23b63501fc4b0e666b57e290
protonio git-H <profiloh@mail.ru>
2023-11-27 00:58:17 +0300

    desktop

--- app/js/swipers.js
+++ app/js/swipers.js
@@ -1,17 +1,77 @@
-// import Swiper JS
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
+let swiper = null;
+let swiper2 = null;
+let swiper3 = null;
 
-const swiper = new Swiper('.swiper',{
-    slidesPerView: 3,
-    spaceBetween: 20,
-}
+function swiperOneInit(){
+    if(!swiper){
+        swiper = new Swiper('.swiper', {
+            
+            direction: 'horizontal',
+            
+            spaceBetween: 20,
+            
+        });
+    }  
+      
+};
+
+function swiperTwoInit(){
+    if (!swiper2){
+        swiper2 = new Swiper('.swiper2', {
+            
+            direction: 'horizontal',
+            
+            
+        });  
+    }
+};
+
+function swiperThreeInit(){
+    if (!swiper3){
+        swiper3 = new Swiper('.swiper3', {
+            direction: 'horizontal',
+            
+            slidesPerView:4,
+            
+        });  
+    }
+};
+
+
+if (document.documentElement.clientWidth < 768){
     
-);
+    swiperOneInit();
+    swiperTwoInit();
+    swiperThreeInit();
+};
 
 
-const swiper2 = new Swiper('.blog__list-wrapper.swiper',{
-    slidesPerView: 1,
-}
+function swiperDestroy() {
+    if (swiper && swiper2 && swiper3){
+        swiper.destroy();
+        swiper2.destroy();
+        swiper3.destroy();
+    }
+};
+
+
+
+
+window.addEventListener('resize',(e) =>{
     
-);
+  if (document.documentElement.clientWidth <= 768) {
+    
+    swiperOneInit();
+    swiperTwoInit();
+    swiperThreeInit();
+    
+  } else {
+    
+    swiperDestroy();
+  }
+    
+});
+
+
